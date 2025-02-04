36784e4387dbc8f309d4f6d1774029a33f860d53
Gustavo A. Verneck <gustavoverneck@gmail.com>
2025-02-04 13:38:31 -0300

    Add files via upload

---
+++ examples/simple.py
@@ -0,0 +1,9 @@
+from LatteLabLib import *
+
+def main():
+    config = Config(total_timesteps=10000, window_dimensions=(1280,720), grid_size=(64,64,64)).get()
+    lbm = LBM(config)
+    lbm.run()
+
+if __name__ == '__main__':   
+    main()
